8159b03fd18f83fda1d6d8cc4e2fa45dce063f88
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2025-08-17 10:15:48 +0200

    Setting default values

--- react/projects/01-starting-project/src/components/Tabs.jsx
+++ react/projects/01-starting-project/src/components/Tabs.jsx
@@ -1,5 +1,6 @@
 // Wrapper component
-export default function Tabs({ children, buttons, ButtonsContainer }) {
+// We can use defaults, setting them directly
+export default function Tabs({ children, buttons, ButtonsContainer = "menu" }) {
     // If it's a string React will search for a built-in element
     // Otherwise will render a created component
 	return (
