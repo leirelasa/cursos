4d5502511d6735bb0282296b8c6d2770e33c1385
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2025-08-17 13:00:20 +0200

    Set up the player name bar

---
+++ react/projects/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+	const [isEditing, setisEditing] = useState(false);
+	const [name, setName] = useState(initialName);
+
+	const input = (
+		<input
+			required
+			type="text"
+			className="player-name"
+			value={name}
+			onChange={handleChange}
+		/>
+	);
+
+	const span = <span className="player-name">{name}</span>;
+
+	let nameContainer = isEditing ? input : span;
+	let buttonCaption = isEditing ? "Save" : "Edit";
+
+	function handleEditClick() {
+		//console.log(`Button Edit of player ${name} clicked.`);
+        // Best practice to pass a function, not directly !isEditing because does not evalute at the moment, schedules an update of the state
+		setisEditing((editing) => !editing);
+	}
+
+    function handleChange(event) {
+        //console.log(event);
+        setName(event.target.value)
+    }
+
+	return (
+		<li>
+			<span className="player">
+				{nameContainer}
+				<span className="player-symbol">{symbol}</span>
+			</span>
+			<button onClick={handleEditClick}>{buttonCaption}</button>
+		</li>
+	);
+}
