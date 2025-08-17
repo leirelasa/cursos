3a190802a8d1a848a449d487b6ee29a401b2b634
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2025-08-17 18:54:31 +0200

    Updating the array of the game board inmutably

--- react/projects/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ react/projects/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,17 +1,41 @@
+import { useState } from "react";
 const initialGameBoard = [
 	[null, null, null],
 	[null, null, null],
 	[null, null, null],
 ];
 export default function GameBoard() {
-    // We'll iterate over the main array first and then over every array
+	const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+	// Inmutable way of updating
+	function handleSelectSquare(row, column) {
+		setGameBoard((prevGameBoard) => {
+			// Working with a new reference, creating a copy of the array
+			const updatedGameBoard = [
+				...prevGameBoard.map((innerArray) => [...innerArray]),
+			];
+
+			updatedGameBoard[row][column] = "X";
+			return updatedGameBoard;
+		});
+	}
+
+	// We'll iterate over the main array first and then over every array
 	return (
 		<ol id="game-board">
-			{initialGameBoard.map((row, rowId) => (
+			{gameBoard.map((row, rowId) => (
 				<li key={rowId}>
 					<ol>
 						{row.map((column, columnId) => (
-							<li key={columnId}><button>{column}</button></li>
+							<li key={columnId}>
+								<button
+									onClick={() =>
+										handleSelectSquare(rowId, columnId)
+									}
+								>
+									{column}
+								</button>
+							</li>
 						))}
 					</ol>
 				</li>
